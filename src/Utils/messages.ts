2c7e46bd4d4ed21102829db80239aec58d981b0f
Pokoke-01 <pokokemng1@gmail.com>
2021-12-31 17:04:26 +0700

    use the ListMessageListType enum

--- src/Utils/messages.ts
+++ src/Utils/messages.ts
@@ -323,7 +323,7 @@ export const generateWAMessageContent = async(
 			title: message.title,
 			footerText: message.footer,
 			description: message.text,
-			listType: 1
+			listType: proto.ListMessage.ListMessageListType[1]
 		}
 
 		m = { listMessage }
